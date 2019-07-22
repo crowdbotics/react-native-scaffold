ea75afbe0c488d4872d099fc2bccc7b11da8098e
tanduong <tanduong@live.com>
2019-07-22 08:25:36 -0500

    android setup

--- {{cookiecutter.project_slug}}/android/app/src/main/java/com/{{cookiecutter.project_slug}}/MainApplication.java
+++ {{cookiecutter.project_slug}}/android/app/src/main/java/com/{{cookiecutter.project_slug}}/MainApplication.java
@@ -8,6 +8,9 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;                       
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +27,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGestureHandlerPackage()
+          new RNGestureHandlerPackage(),
+          new RNFirebasePackage(),
+          new RNFirebaseMessagingPackage(),
+          new RNFirebaseNotificationsPackage()
       );
     }
 

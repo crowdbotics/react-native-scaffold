96793b62083b1404f712bc36d8b53d4c82cc704c
Emmanuel Orozco <indi3.rok@gmail.com>
2019-07-26 17:45:40 +0200

    TECH : Add calendar scaffold

--- {{cookiecutter.project_slug}}/android/app/src/main/java/com/{{cookiecutter.project_slug}}/MainApplication.java
+++ {{cookiecutter.project_slug}}/android/app/src/main/java/com/{{cookiecutter.project_slug}}/MainApplication.java
@@ -3,6 +3,7 @@ package com.{{cookiecutter.project_slug}};
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+{% if cookiecutter.has_calendar_blueprint == "True" %}import co.apptailor.googlesignin.RNGoogleSigninPackage;{% endif %}
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            {% if cookiecutter.has_calendar_blueprint == "True" %}new RNGoogleSigninPackage(),{% endif %}
             new RNGestureHandlerPackage()
       );
     }

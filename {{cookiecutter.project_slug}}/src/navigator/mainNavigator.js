dfbaae199a1de5c3f2906f65319932b5376ebdb7
Colin Zhou <colinzhou@berkeley.edu>
2019-07-30 14:24:10 -0700

    test

--- {{cookiecutter.project_slug}}/src/navigator/mainNavigator.js
+++ {{cookiecutter.project_slug}}/src/navigator/mainNavigator.js
@@ -4,6 +4,7 @@ import SplashScreen from "../features/SplashScreen";
 
 {% if cookiecutter.has_login_blueprint %}import { EmailAuthNavigator } from "../features/EmailAuth/navigator";{% endif %}
 {% if cookiecutter.has_maps_blueprint == "y" %}import { GoogleMapsNavigator } from "../features/MapsScreen/navigator";{% endif %}
+{% if cookiecutter.has_camera_blueprint == "y" %}import { CameraNavigator } from "../features/UserCamera/navigator";{% endif %}
 /**
  * new navigators can be imported here
  */

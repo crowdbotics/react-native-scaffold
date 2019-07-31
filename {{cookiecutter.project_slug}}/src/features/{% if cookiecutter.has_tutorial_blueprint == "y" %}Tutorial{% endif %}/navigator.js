f2b7cf7dce8d775fde2a377385d33e1874fdd0ef
Emmanuel Orozco <indi3.rok@gmail.com>
2019-07-31 16:31:49 +0200

    Added tutorial blueprint

---
+++ {{cookiecutter.project_slug}}/src/features/{% if cookiecutter.has_tutorial_blueprint == "y" %}Tutorial{% endif %}/navigator.js
@@ -0,0 +1,12 @@
+import { createStackNavigator } from "react-navigation";
+
+import MainTutorialScreen from "./screens/MainTutorial";
+
+export default createStackNavigator(
+  {
+    MainTutorial: { screen: MainTutorialScreen }
+  },
+  {
+    initialRouteName: "MainTutorial"
+  }
+);

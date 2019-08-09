a69fb4fcdbc9646fdfed40fcaa729454cbe3fc3e
Colin Zhou <colinzhou@berkeley.edu>
2019-08-09 05:50:42 -0700

    Added PubNub for backend

--- {{cookiecutter.project_slug}}/src/features/{% if cookiecutter.has_messenger_blueprint == "y" %}Messenger{% endif %}/screens/ChatScreen/index.js
+++ {{cookiecutter.project_slug}}/src/features/{% if cookiecutter.has_messenger_blueprint == "y" %}Messenger{% endif %}/screens/ChatScreen/index.js
@@ -1,36 +1,47 @@
 import React from 'react'
 import { GiftedChat } from 'react-native-gifted-chat'
+import PubNubReact from 'pubnub-react';
 
 export default class ChatScreen extends React.Component {
 
   constructor(props) {
-   super(props);
-   state = {
+    super(props);
+    this.pubnub = new PubNubReact({
+      publishKey: 'pub-c-09038c51-029c-47a7-95f2-b6c7407afb12',
+      subscribeKey: 'sub-c-6b1bee04-ba9f-11e9-8753-ce76e7dc5905'
+    });
+    this.pubnub.init(this);
+    state = {
      messages: [],
-   }
- }
+    }
+    this.id = this.randomid();
+  }
+  randomid = () => {
+    return Math.floor(Math.random() * 100);
+  };
+
+  componentWillUnmount() {
+    this.pubnub.unsubscribe({ channels: ['ReactChat'] });
+  }
 
   componentWillMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Greetings!',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://cdn-images-1.medium.com/max/1200/1*D0E03PmeVga1wLJt7YLd1w.png',
-          },
-        },
-      ],
-    })
+    this.pubnub.subscribe({
+      channels: ["ReactChat"],
+      message: message => console.log("sub", message),
+    });
+
+    this.pubnub.getMessage("ReactChat", m => {
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, m["message"]),
+      }));
+    });
   }
 
   onSend(messages = []) {
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }))
+    this.pubnub.publish({
+      message: messages,
+      channel: "ReactChat",
+    });
   }
 
   render() {
@@ -39,7 +50,9 @@ export default class ChatScreen extends React.Component {
         messages={this.state.messages}
         onSend={messages => this.onSend(messages)}
         user={ {
-          _id: 1,
+          _id: this.id,
+          name: 'React Native',
+          avatar: 'https://cdn-images-1.medium.com/max/1200/1*D0E03PmeVga1wLJt7YLd1w.png',
         }}
       />
     )

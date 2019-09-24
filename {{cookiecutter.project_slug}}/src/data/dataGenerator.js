61e788e350fae01a3f8e2afd0b11631a6fedfbfc
tanduong <tanduong@live.com>
2019-09-24 09:33:22 -0500

    optimize lodash usage

--- {{cookiecutter.project_slug}}/src/data/dataGenerator.js
+++ {{cookiecutter.project_slug}}/src/data/dataGenerator.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import users from './raw/users';
 import articles from './raw/articles';
 import notifications from './raw/notifications';
@@ -8,11 +7,12 @@ const populateArticles = () => {
   articles.forEach(article => {
     const userArticle = article;
     const userId = articles.indexOf(article) % users.length;
-    userArticle.user = _.find(users, x => x.id === userId) || users[0];
+    userArticle.user = users.find(x => x.id === userId) || users[0];
+
     userArticle.comments.map(comment => {
       const userComment = comment;
       const commentUserId = article.comments.indexOf(comment) % users.length;
-      userComment.user = _.find(users, x => x.id === commentUserId) || users[0];
+      userComment.user = users.find(x => x.id === commentUserId) || users[0];
       return userComment;
     });
     return userArticle;
@@ -23,7 +23,7 @@ const populateNotifications = () => {
   notifications.map(notification => {
     const userNotification = notification;
     const userId = notifications.indexOf(notification) % users.length;
-    userNotification.user = _.find(users, x => x.id === userId) || users[0];
+    userNotification.user = users.find(x => x.id === userId) || users[0];
     return userNotification;
   });
 };
@@ -31,7 +31,7 @@ const populateNotifications = () => {
 const populateConversations = () => {
   conversations.map(conversation => {
     const userConversation = conversation;
-    userConversation.withUser = _.find(users, x => x.id === conversation.withUserId) || users[0];
+    userConversation.withUser = users.find(x => x.id === conversation.withUserId) || users[0];
     return userConversation;
   });
 };

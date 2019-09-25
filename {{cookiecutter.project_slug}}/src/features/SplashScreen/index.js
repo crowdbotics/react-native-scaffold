cca5fb6f9596db4efdf14f5ecf96202bbef04791
Emmanuel Orozco <indi3.rok@gmail.com>
2019-09-25 17:42:25 +0300

    added default icon

--- {{cookiecutter.project_slug}}/src/features/SplashScreen/index.js
+++ {{cookiecutter.project_slug}}/src/features/SplashScreen/index.js
@@ -1,35 +1,40 @@
-import React, { Component } from "react";
-import { StyleSheet, Image, ScrollView, TouchableOpacity } from "react-native";
-import { Text, Button } from "react-native-ui-kitten";
+import React, {Component} from 'react';
+import {StyleSheet, Image, ScrollView, TouchableOpacity} from 'react-native';
+import {Text, Button} from 'react-native-ui-kitten';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-import LogoIcon from "../../assets/images/backgroundLoginV1.png";
-import { installed_blueprints } from "../../config/installed_blueprints";
-import { store } from "../../redux/store";
-import { styles } from './styles'
+import LogoIcon from '../../assets/images/backgroundLoginV1.png';
+import {installed_blueprints} from '../../config/installed_blueprints';
+import {store} from '../../redux/store';
+import {styles} from './styles';
 
 export default class App extends Component {
-
   static navigationOptions = {
     title: 'Installed blueprints',
   };
 
   componentDidMount() {
-    store.dispatch({ type: "TEST/ALO" });
+    store.dispatch({type: 'TEST/ALO'});
   }
 
   renderItems() {
     const {
-      navigation: { navigate }
+      navigation: {navigate},
     } = this.props;
 
     return installed_blueprints.map(item => {
-      if (item.hasOwnProperty("access_route")) {
+      if (item.hasOwnProperty('access_route')) {
         return (
-          <TouchableOpacity onPress={_ => navigate(item.access_route)}
-            style={styles.item} key={`${item.name}--blueprint-button`}
-          >
-            <Icon style={styles.itemLogo} name={item.icon_name} size={40} color="#F88087" />
+          <TouchableOpacity
+            onPress={_ => navigate(item.access_route)}
+            style={styles.item}
+            key={`${item.name}--blueprint-button`}>
+            <Icon
+              style={styles.itemLogo}
+              name={item.icon_name ? item.icon_name : 'profiles'}
+              size={40}
+              color="#F88087"
+            />
             <Text style={styles.itemFont}>{item.human_name}</Text>
           </TouchableOpacity>
         );

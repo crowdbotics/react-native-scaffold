081c6c09a1b0099b2580a97c5c19b193fadec4c7
Daniel <daniel.s@crowdbotics.com>
2020-07-01 16:44:05 +0100

    Configure app to use new store

---
+++ {{cookiecutter.project_slug}}/src/store/index.js
@@ -0,0 +1,28 @@
+import { apiReducer } from "./reducers";
+import { combineReducers } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./sagas";
+
+const sagaMiddleware = createSagaMiddleware();
+
+/**
+ * this app uses React Native Debugger, but it works without it
+ */
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const middlewares = [sagaMiddleware /** more middlewares if any goes here */];
+
+const store = createStore(
+  combineReducers({
+      apiReducer: (state, action) => {
+          if (state == null) state = [];
+          return state
+      }
+  })
+  composeEnhancers(applyMiddleware(...middlewares))
+);
+
+sagaMiddleware.run(rootSaga);
+
+export { store };

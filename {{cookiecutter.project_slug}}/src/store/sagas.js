d1c816a09e83062ea41bb7262e310751a65aa005
Daniel Sousa <daniel.s@crowdbotics.com>
2020-10-14 16:51:01 +0100

    Add missing actions import to sagas

--- {{cookiecutter.project_slug}}/src/store/sagas.js
+++ {{cookiecutter.project_slug}}/src/store/sagas.js
@@ -1,6 +1,7 @@
 import {put, call, all, spawn, takeEvery} from 'redux-saga/effects';
 import {apiService} from './services';
 import * as types from './constants';
+import * as actions from './actions';
 
 export default function* rootSaga() {
   const sagas = [

8fc15cf4c549cc257d8a2907d057ee4a4870fffd
Daniel Sousa <mail@danielsousa.io>
2023-09-27 15:13:05 +0100

    remove existing scaffold

--- {{cookiecutter.project_slug}}/src/config/app.js
+++
@@ -1,5 +0,0 @@
-export const appConfig = {
-  // todo add library to handle env variables
-  emailAuthAPIEndPoint: "https://{{cookiecutter.project_dash_slug}}.botics.co",
-  defaultTimeout: 5000
-};
